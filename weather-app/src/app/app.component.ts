6ed4ff2c3ec2cf207f507a45b2a9ac43fbbb32c8
CassandraBeauchamp <150950665+CassandraBeauchamp@users.noreply.github.com>
2024-01-03 11:19:57 -0600

    Made the main page link to the weather component using router-outlet

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {RouterModule} from '@angular/router';
+import { WeatherComponent } from "./weather/weather.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [WeatherComponent, RouterOutlet, RouterLink],
+  template: `
+    <main>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
