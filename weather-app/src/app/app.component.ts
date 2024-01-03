46e46d36e96b5d23c28c7400fde85da75c7f018e
CassandraBeauchamp <150950665+CassandraBeauchamp@users.noreply.github.com>
2024-01-03 11:58:51 -0600

    Wired button to be able to accept values put into the search weather field

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {RouterModule} from '@angular/router';
 import { WeatherComponent } from "./weather/weather.component";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [WeatherComponent, RouterOutlet, RouterLink],
+  imports: [WeatherComponent, RouterOutlet, RouterLink, ReactiveFormsModule],
   template: `
     <main>
       <section class="content">
--- weather-app/src/app/weather/weather.component.ts
+++ weather-app/src/app/weather/weather.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {NgIf} from "@angular/common";
+
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
 })
-export class WeatherComponent {
+export class WeatherComponent implements OnInit{
 
+  public weatherSearchForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.weatherSearchForm = formBuilder.group(0);
+  }
+  ngOnInit() {
+    this.weatherSearchForm = this.formBuilder.group({
+      location: ['']
+    });
+  }
+  sendToAPIXU(formValues: any) {
+    console.log(formValues);
+  }
 }
+
